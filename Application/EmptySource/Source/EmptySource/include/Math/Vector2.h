69de1dae52f517abbdfaf76abab0131eedf6df7e
Gonzalo Ceciliano <manchitas.001@hotmail.com>
2019-03-14 03:14:56 -0600

    More functionality to Vector2

--- Application/EmptySource/Source/EmptySource/include/Math/Vector2.h
+++ Application/EmptySource/Source/EmptySource/include/Math/Vector2.h
@@ -24,8 +24,13 @@ public:
 	HOST_DEVICE inline void Normalize();
 	HOST_DEVICE inline Vector2 Normalized() const;
 	
+	// A special version of the cross product for 2D vectors (returns scalar value).
 	HOST_DEVICE FORCEINLINE float Cross(const Vector2& Other) const;
+	HOST_DEVICE FORCEINLINE static float Cross(const Vector2 &A, const Vector2 &B);
 	HOST_DEVICE FORCEINLINE float Dot(const Vector2& Other) const;
+	HOST_DEVICE FORCEINLINE static float Dot(const Vector2 &A, const Vector2 &B);
+	HOST_DEVICE FORCEINLINE Vector2 Orthogonal(bool Polarity) const;
+	HOST_DEVICE FORCEINLINE Vector2 Orthonormal(bool Polarity) const;
 	HOST_DEVICE FORCEINLINE static Vector2 Lerp(const Vector2& Start, const Vector2& End, float t);
 	
 	HOST_DEVICE inline float & operator[](unsigned int i);
@@ -33,6 +38,7 @@ public:
 	HOST_DEVICE inline const float* PointerToValue() const;
 	
 	HOST_DEVICE FORCEINLINE bool operator==(const Vector2& Other) const;
+	HOST_DEVICE FORCEINLINE bool operator!() const;
 	HOST_DEVICE FORCEINLINE bool operator!=(const Vector2& Other) const;
 	
 	HOST_DEVICE FORCEINLINE Vector2 operator+(const Vector2& Other) const;
@@ -49,6 +55,11 @@ public:
 	HOST_DEVICE FORCEINLINE Vector2& operator/=(const Vector2& Other);
 	HOST_DEVICE FORCEINLINE Vector2& operator*=(const float& Value);
 	HOST_DEVICE FORCEINLINE Vector2& operator/=(const float& Value);
+
+	HOST_DEVICE inline friend Vector2 operator*(float Value, const Vector2 &Vector);
+	HOST_DEVICE inline friend Vector2 operator/(float Value, const Vector2 &Vector);
 };
 
 #include "..\Math\Vector2.inl"
+
+typedef Vector2 Point2;
