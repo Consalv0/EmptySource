fc73809bd8659d961ff572d534818dfc875f0339
Gonzalo Ceciliano <manchitas.001@hotmail.com>
2019-03-17 19:50:32 -0600

    Packing !Testing

--- Application/EmptySource/Source/EmptySource/source/Text2DGenerator.cpp
+++ Application/EmptySource/Source/EmptySource/source/Text2DGenerator.cpp
@@ -88,30 +88,59 @@ bool Text2DGenerator::GenerateGlyphAtlas(Bitmap<unsigned char> & Atlas) {
 		// }
 	}
 
-	IntVector2 AtlasPosition;
-	for (TDictionary<unsigned long, FontGlyph*>::const_iterator Begin = LoadedCharacters.begin(); Begin != LoadedCharacters.end(); Begin++) {
-		FontGlyph * Character = Begin->second;
-		int IndexPos = AtlasPosition.x + AtlasPosition.y * AtlasSize;
-		IndexPos += (int)Character->Bearing.x;
+	TArray<FontGlyph *> GlyphArray = TArray<FontGlyph * >(LoadedCharacters.size());
+	// --- Sort by perimeter
+	{
+		size_t Count = 0;
+		for (TDictionary<unsigned long, FontGlyph*>::const_iterator Begin = LoadedCharacters.begin(); Begin != LoadedCharacters.end(); Begin++)
+			GlyphArray[Count++] = Begin->second;
 
-		// --- Asign the current UV Position
-		Character->UV.xMin = (Character->Bearing.x + AtlasPosition.u) / (float)AtlasSize;
-		Character->UV.xMax = (Character->Bearing.x + AtlasPosition.u + Character->Width + PixelRange * 2) / (float)AtlasSize;
-		Character->UV.yMin = (AtlasPosition.y) / (float)AtlasSize;
-		Character->UV.yMax = (AtlasPosition.y + Character->Height + PixelRange * 2) / (float)AtlasSize;
-		
-		// --- If current position exceds the canvas with the next character, reset the position in Y
-		AtlasPosition.y += Character->SDFResterized.GetHeight();
-		if (AtlasPosition.y * AtlasSize > (AtlasSizeSqr - Character->SDFResterized.GetHeight() * AtlasSize)) {
-			AtlasPosition.x += Character->SDFResterized.GetHeight();
-			AtlasPosition.y = 0;
+		std::sort(GlyphArray.begin(), GlyphArray.end(), [](FontGlyph * A, FontGlyph * B) {
+			return (
+				A->SDFResterized.GetHeight() * 2 + A->SDFResterized.GetWidth() * 2 >
+				B->SDFResterized.GetHeight() * 2 + B->SDFResterized.GetWidth() * 2
+			);
+		}); 
+	}
+
+	TArray<BoundingBox2D> EmptySpaces;
+	EmptySpaces.push_back({0.F, 0.F, (float)AtlasSize, (float)AtlasSize});
+
+	IntVector2 AtlasPosition;
+	for (TArray<FontGlyph * >::const_iterator Begin = GlyphArray.begin(); Begin != GlyphArray.end(); Begin++) {
+		FontGlyph * Character = *Begin;
+
+		int CandidateSpaceIndex = -1;
+		BoundingBox2D * BBox = NULL;
+		for (TArray<BoundingBox2D>::const_iterator Back = EmptySpaces.end(); Back != EmptySpaces.begin(); Back--) {
+			CandidateSpaceIndex++;
+			BBox = &EmptySpaces[CandidateSpaceIndex];
+			if (Character->SDFResterized.GetWidth() < (int)BBox->GetWidth() &&
+				Character->SDFResterized.GetHeight() < (int)BBox->GetHeight())
+				break;
 		}
+
+		if (BBox == NULL) {
+			Debug::Log(Debug::LogDebug, L"Error writting in %c", Character->UnicodeValue);
+			continue;
+		}
+		int Width = Character->SDFResterized.GetWidth();
+		int Height = Character->SDFResterized.GetHeight();
+		
+		// --- Asign the current UV Position
+		Character->UV.xMin = (BBox->xMin) / (float)AtlasSize;
+		Character->UV.xMax = (BBox->xMin + Character->SDFResterized.GetWidth()) / (float)AtlasSize;
+		Character->UV.yMin = (BBox->yMin) / (float)AtlasSize;
+		Character->UV.yMax = (BBox->yMin + Character->SDFResterized.GetHeight()) / (float)AtlasSize;
+		
+		int IndexPos = int(BBox->xMin + BBox->yMin * AtlasSize);
 		
 		// --- Render current character in the current position
 		for (int i = 0; i < Character->SDFResterized.GetHeight(); ++i) {
 			for (int j = 0; j < Character->SDFResterized.GetWidth(); ++j) {
 				if (IndexPos >= AtlasSizeSqr) {
 					IndexPos -= AtlasSizeSqr / Character->SDFResterized.GetHeight();
+					Debug::Log(Debug::LogDebug, L"Error writting in %c", Character->UnicodeValue);
 				}
 				if (IndexPos < AtlasSizeSqr && IndexPos >= 0) {
 					Value = Math::Clamp(int(Character->SDFResterized[i * Character->SDFResterized.GetWidth() + j] * 0x100), 0xff);
@@ -121,6 +150,25 @@ bool Text2DGenerator::GenerateGlyphAtlas(Bitmap<unsigned char> & Atlas) {
 			}
 			IndexPos += -Character->SDFResterized.GetWidth() + AtlasSize;
 		}
+
+		int DeltaWidth = (int)BBox->GetWidth() - Width;
+		int DeltaHeight = (int)BBox->GetHeight() - Height;
+		BoundingBox2D BiggerSplit, SmallerSplit;
+
+		if (DeltaWidth < DeltaHeight) {
+			BiggerSplit =  { BBox->xMin,         BBox->yMin + Height, BBox->xMax, BBox->yMax};
+			SmallerSplit = { BBox->xMin + Width, BBox->yMin,          BBox->xMax, BBox->yMin + Height };
+		}
+		else {
+			BiggerSplit =  { BBox->xMin + Width, BBox->yMin,          BBox->xMax,         BBox->yMax };
+			SmallerSplit = { BBox->xMin,         BBox->yMin + Height, BBox->xMin + Width, BBox->yMax };
+		}
+
+		EmptySpaces[CandidateSpaceIndex] = EmptySpaces.back();
+		EmptySpaces.pop_back();
+
+		EmptySpaces.push_back(BiggerSplit);
+		EmptySpaces.push_back(SmallerSplit);
 	}
 
 	return true;
