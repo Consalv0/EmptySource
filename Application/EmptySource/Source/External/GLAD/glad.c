cab9ad5d3a83e19448f482c6b790e5c9d60f4d9a
Gonzalo Ceciliano <manchitas.001@hotmail.com>
2019-03-19 20:35:19 -0600

    Removed include and src folder

--- Application/EmptySource/Source/External/GLAD/glad.c
+++ Application/EmptySource/Source/External/GLAD/glad.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <External/GLAD/include/glad.h>
+#include "GLAD\include\glad.h"
 
 static void* get_proc(const char *namez);
 
