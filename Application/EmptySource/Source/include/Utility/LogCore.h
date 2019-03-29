5f422bf290bc1a3094b37995c70339234d6d6b0a
Gonzalo Ceciliano <manchitas.001@hotmail.com>
2019-03-28 20:48:21 -0600

    Multi filter

--- Application/EmptySource/Source/include/Utility/LogCore.h
+++ Application/EmptySource/Source/include/Utility/LogCore.h
@@ -41,37 +41,33 @@ namespace Debug {
 			return;
 		}
 
-        WString LogText = L"";
+        WString LogText;
         
 #ifdef WIN32
-        HANDLE hstdin = GetStdHandle(STD_INPUT_HANDLE); \
-        HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE); \
-        CONSOLE_SCREEN_BUFFER_INFO csbi; \
-        GetConsoleScreenBufferInfo(hstdout, &csbi); \
+        HANDLE hstdin = GetStdHandle(STD_INPUT_HANDLE);
+        HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(hstdout, &csbi);
         
-        switch (Filter) {
-            case LogNormal:   LogText += L"[LOG] "; break;
-			case LogInfo:     LogText += L"[INFO] "; SetConsoleTextAttribute(hstdout, FOREGROUND_INTENSITY ); break;
-            case LogWarning:  LogText += L"[WARNING] "; SetConsoleTextAttribute(hstdout, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY); break;
-            case LogError:    LogText += L"[ERROR] "; SetConsoleTextAttribute(hstdout, FOREGROUND_RED | FOREGROUND_INTENSITY); break;
-            case LogCritical: LogText += L"[CRITICAL] "; SetConsoleTextAttribute(hstdout, FOREGROUND_RED); break;
-            case LogDebug:    LogText += L"[DEBUG] "; SetConsoleTextAttribute(hstdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY); break;
-        }
+        if ((Filter & LogNormal & LogFilter::Value)   != NoLog) { LogText = L"[LOG] "; }
+		if ((Filter & LogInfo & LogFilter::Value)     != NoLog) { LogText = L"[INFO] ";     SetConsoleTextAttribute(hstdout, FOREGROUND_INTENSITY); }
+        if ((Filter & LogDebug & LogFilter::Value)    != NoLog) { LogText = L"[DEBUG] ";    SetConsoleTextAttribute(hstdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY); }
+		if ((Filter & LogWarning & LogFilter::Value)  != NoLog) { LogText = L"[WARNING] ";  SetConsoleTextAttribute(hstdout, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY); }
+		if ((Filter & LogError & LogFilter::Value)    != NoLog) { LogText = L"[ERROR] ";    SetConsoleTextAttribute(hstdout, FOREGROUND_RED | FOREGROUND_INTENSITY); }
+		if ((Filter & LogCritical & LogFilter::Value) != NoLog) { LogText = L"[CRITICAL] "; SetConsoleTextAttribute(hstdout, FOREGROUND_RED); }
 
 		setlocale(LC_ALL, "en_US.UTF-8");
         std::wprintf((LogText + (Text + L"\n")).c_str(), Args ...);
         SetConsoleTextAttribute(hstdout, csbi.wAttributes);
 #else
         
-        switch (Filter) {
-            case LogNormal:   LogText += L"\033[40m[LOG]\033[0m "; break;
-			case LogInfo:     LogText += L"\033[90;40m[INFO]\033[90;49m "; break;
-            case LogWarning:  LogText += L"\033[33;40m[WARNING]\033[33;49m "; break;
-            case LogError:    LogText += L"\033[31;40m[ERROR]\033[31;49m "; break;
-            case LogCritical: LogText += L"\033[31;40m[CRITICAL]\033[31;49m "; break;
-            case LogDebug:    LogText += L"\033[32;40m[DEBUG]\033[32;49m "; break;
-        }
-        
+        if ((Filter & LogNormal & LogFilter::Value)   != NoLog) LogText = L"\033[40m[LOG]\033[0m ";
+		if ((Filter & LogInfo & LogFilter::Value)     != NoLog) LogText = L"\033[90;40m[INFO]\033[90;49m ";
+        if ((Filter & LogDebug & LogFilter::Value)    != NoLog) LogText = L"\033[33;40m[WARNING]\033[33;49m ";
+        if ((Filter & LogWarning & LogFilter::Value)  != NoLog) LogText = L"\033[31;40m[ERROR]\033[31;49m ";
+        if ((Filter & LogError & LogFilter::Value)    != NoLog) LogText = L"\033[31;40m[CRITICAL]\033[31;49m ";
+        if ((Filter & LogCritical & LogFilter::Value) != NoLog) LogText = L"\033[32;40m[DEBUG]\033[32;49m ";
+
         setlocale(LC_ALL, "en_US.UTF-8");
         std::wprintf((LogText + (Text + L"\033[0m\n")).c_str(), Args ...);
 #endif
