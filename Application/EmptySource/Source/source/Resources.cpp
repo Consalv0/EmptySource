ab0d9500ce0c6bda4dd442a90b97d2864d2dc668
Gonzalo Ceciliano <manchitas.001@hotmail.com>
2019-05-20 14:07:35 -0500

    GetResourcesFile

--- Application/EmptySource/Source/source/Resources.cpp
+++ Application/EmptySource/Source/source/Resources.cpp
@@ -8,13 +8,49 @@
 
 TDictionary<size_t, BaseResource*> ResourceManager::Resources = TDictionary<size_t, BaseResource*>();
 
-BaseResource::BaseResource(const WString & FilePath) : IIdentifier(FilePath), FilePath(FilePath), isDone(false) {
+BaseResource::BaseResource(const WString & Name, const size_t & GUID) : GUID(GUID), Name(Name), isDone(false) {
 }
 
-bool BaseResource::IsDone() {
+bool BaseResource::IsDone() const {
 	return isDone;
 }
 
-const FileStream * BaseResource::GetFile() {
-	return FileManager::GetFile(FilePath);
+size_t BaseResource::GetIdentifier() const {
+	return GUID;
+}
+
+const FileStream * BaseResource::GetFile() const {
+	return FileManager::GetFile(Name);
+}
+
+FileStream * ResourceManager::GetResourcesFile() {
+	static WString ResourceFilePath = L"Resources/Resouces.yaml";
+	FileStream * ResourceFile = FileManager::GetFile(ResourceFilePath);
+	YAML::Node BaseNode;
+	bool bNeedsModification = false;
+	if (ResourceFile == NULL) {
+		ResourceFile = FileManager::MakeFile(ResourceFilePath);
+		bNeedsModification = true;
+	}
+	else {
+		String FileInfo;
+		if (!ResourceFile->ReadNarrowStream(&FileInfo))
+			return false;
+		BaseNode = YAML::Load(FileInfo.c_str());
+		if (!BaseNode["Resources"].IsDefined()) {
+			bNeedsModification = true;
+		}
+	}
+
+	if (bNeedsModification && ResourceFile->IsValid()) {
+		ResourceFile->Clean();
+		YAML::Node ResourcesNode;
+		BaseNode["Resources"] = ResourcesNode;
+		YAML::Emitter Out;
+		Out << BaseNode;
+		(*ResourceFile) << Out.c_str();
+		ResourceFile->Close();
+	}
+
+	return ResourceFile;
 }
